b60e9599724a57c44dd85e68ca251dc4809a1400
DESKTOP-CQ10NM0\jumpsoo <jumpsoo42@gmail.com>
2022-07-13 14:24:26 +0900

    누락 파일

---
+++ lib/domain/use_case/tb_wh_item/select_tb_wh_item.dart
@@ -0,0 +1,15 @@
+import 'package:kdlwms/data/data_source/result.dart';
+import 'package:kdlwms/domain/model/tb_wh_item.dart';
+import 'package:kdlwms/domain/repository/tb_wh_item_repo.dart';
+
+class SelectTbWhItem{
+
+  final TbWhItemRepo repository;
+  SelectTbWhItem(this.repository);
+
+  Future<Result<TbWhItem?>> call (TbWhItem tbWhItem) async {
+
+    return await repository.selectTbWhItem(tbWhItem);
+  }
+}
+
---
+++ lib/domain/use_case/use_case_tb_wh_item.dart
@@ -0,0 +1,10 @@
+
+import 'package:kdlwms/domain/use_case/tb_wh_item/select_tb_wh_item.dart';
+
+class UseCaseTbWhItem {
+  final SelectTbWhItem selectTbWhItem;
+
+  UseCaseTbWhItem({
+    required this.selectTbWhItem,
+  });
+}
